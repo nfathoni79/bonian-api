739f300590d56cfdb8bd47c20e054eb22de8f035
kinosuke <iwaninfo@gmail.com>
2019-03-11 12:59:07 +0700

    implement address added

--- src/Controller/V1/Web/AddressesController.php
+++ src/Controller/V1/Web/AddressesController.php
@@ -59,8 +59,41 @@ class AddressesController extends AppController
     public function add()
     {
         $this->request->allowMethod(['post', 'put']);
+        $content_type = $this->request->getHeader('content-type');
 
         //process add address here
+
+
+        $allData = $this->request->getData();
+
+        $entity = $this->Customers->CustomerAddreses->newEntity();
+
+        $this->Customers->CustomerAddreses->patchEntity($entity, $allData, [
+            'fields' => [
+                'province_id',
+                'city_id',
+                'subdistrict_id',
+                'is_primary',
+                'title',
+                'recipient_name',
+                'recipient_phone',
+                'latitude',
+                'longitude',
+                'address',
+            ]
+        ]);
+
+        $entity->set('customer_id', $this->Auth->user('id'));
+
+        if ($this->Customers->CustomerAddreses->save($entity)) {
+            //success
+        } else {
+            $this->setResponse($this->response->withStatus(406, 'Failed to add address'));
+            $errors = $entity->getErrors();
+        }
+
+        $this->set(compact('errors'));
+
     }
 
 
