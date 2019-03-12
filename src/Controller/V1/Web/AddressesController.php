216c5460692f86792356330e35af3bae8bd8bc5a
kinosuke <iwaninfo@gmail.com>
2019-03-12 11:05:58 +0700

    add district on address

--- src/Controller/V1/Web/AddressesController.php
+++ src/Controller/V1/Web/AddressesController.php
@@ -116,6 +116,56 @@ class AddressesController extends AppController
     }
 
     /**
+     * delete address given address_id
+     */
+    public function delete()
+    {
+        $this->request->allowMethod(['post', 'put']);
+        if ($address_id = $this->request->getData('address_id')) {
+            $addressEntity = $this->Customers->CustomerAddreses->find()
+                ->where([
+                    'customer_id' => $this->Auth->user('id'),
+                    'id' => $address_id
+                ])
+                ->first();
+
+            if ($addressEntity) {
+                if (!$this->Customers->CustomerAddreses->delete($addressEntity)) {
+                    $this->setResponse($this->response->withStatus(406, 'Failed to delete address'));
+                }
+            }
+
+        }
+    }
+
+    public function view($address_id)
+    {
+        $this->request->allowMethod('get');
+        if ($address_id) {
+            $address = $this->Customers->CustomerAddreses->find()
+                ->where([
+                    'customer_id' => $this->Auth->user('id'),
+                    'CustomerAddreses.id' => $address_id
+                ])
+                ->contain([
+                    'Provinces',
+                    'Cities',
+                    'Subdistricts',
+                ])
+                ->map(function (\App\Model\Entity\CustomerAddrese $row) {
+                    unset($row->customer_id);
+                    $row->created = $row->created instanceof \Cake\I18n\FrozenTime  ? $row->created->timestamp : 0;
+                    $row->modified = $row->modified instanceof \Cake\I18n\FrozenTime  ? $row->modified->timestamp : 0;
+
+                    return $row;
+                })
+                ->first();
+        }
+
+        $this->set(compact('address'));
+    }
+
+    /**
      * get province
      */
     public function getProvince()
@@ -140,6 +190,17 @@ class AddressesController extends AppController
         $city = $city->toArray();
 
         $this->set(compact('city'));
+    }
+
+    public function getDistrict($city_id)
+    {
+        $district = $this->Cities->Subdistricts->find('list')
+            ->where([
+                'city_id' => $city_id
+            ])
+            ->toArray();
+
+        $this->set(compact('district'));
     }
 
 
