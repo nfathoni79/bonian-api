deb34265c463f12965bbe97d7dfdcadad62b399d
kinosuke <iwaninfo@gmail.com>
2019-04-25 18:45:43 +0700

    filter price range api

--- src/Controller/V1/ProductFiltersController.php
+++ src/Controller/V1/ProductFiltersController.php
@@ -9,7 +9,9 @@
 namespace App\Controller\V1;
 
 use App\Controller\V1\AppController as Controller;
+use Cake\Database\Expression\QueryExpression;
 use Cake\Utility\Hash;
+use Cake\Validation\Validator;
 
 /**
  * Class ProductFiltersController
@@ -172,85 +174,101 @@ class ProductFiltersController extends Controller
     {
         $search = $this->request->getQuery('q');
         $category_id = $this->request->getQuery('category_id');
+        $min_price = $this->request->getQuery('min_price');
+        $max_price = $this->request->getQuery('max_price');
 
-        $data = $this->ProductOptionValueLists->find();
+        $validator = $this->_validator();
 
-        $data = $data
-            ->select([
-                'option_values' => "GROUP_CONCAT(ProductOptionValueLists.option_value_id)",
-                'Options.id',
-                'Options.name'
-            ])
-            ->leftJoin(['ProductOptionPrices' => 'product_option_prices'], [
-                'ProductOptionValueLists.product_option_price_id = ProductOptionPrices.id'
-            ])
-            ->leftJoin(['Products' => 'products'], [
-                'ProductOptionPrices.product_id = Products.id'
-            ])
-            ->leftJoin(['ProductToCategories' => 'product_to_categories'], [
-                'ProductOptionPrices.product_id = ProductToCategories.product_id'
-            ])
-            ->leftJoin(['Options' => 'options'], [
-                'ProductOptionValueLists.option_id = Options.id'
-            ])
-            ->leftJoin(['OptionValues' => 'option_values'], [
-                'ProductOptionValueLists.option_value_id = OptionValues.id'
-            ])
-            ->group([
-                'ProductOptionValueLists.option_id'
-            ])
-            ->where([]);
+        $error = $validator->errors($this->request->getQueryParams());
 
-        if ($search) {
-            $data->where([
-                'MATCH (Products.name, Products.highlight) AGAINST (:search IN BOOLEAN MODE)'
-            ])
-            ->bind(':search', $search, 'string');
-        }
+        if (!$error) {
+            $data = $this->ProductOptionValueLists->find();
 
-        if ($category_id) {
-            $descendants = $this->ProductCategories->find('children', ['for' => $category_id])
-                ->toArray();
-            $children = Hash::extract($descendants, '{n}.id');
-            if ($children) {
+            $data = $data
+                ->select([
+                    'option_values' => "GROUP_CONCAT(ProductOptionValueLists.option_value_id)",
+                    'Options.id',
+                    'Options.name'
+                ])
+                ->leftJoin(['ProductOptionPrices' => 'product_option_prices'], [
+                    'ProductOptionValueLists.product_option_price_id = ProductOptionPrices.id'
+                ])
+                ->leftJoin(['Products' => 'products'], [
+                    'ProductOptionPrices.product_id = Products.id'
+                ])
+                ->leftJoin(['ProductToCategories' => 'product_to_categories'], [
+                    'ProductOptionPrices.product_id = ProductToCategories.product_id'
+                ])
+                ->leftJoin(['Options' => 'options'], [
+                    'ProductOptionValueLists.option_id = Options.id'
+                ])
+                ->leftJoin(['OptionValues' => 'option_values'], [
+                    'ProductOptionValueLists.option_value_id = OptionValues.id'
+                ])
+                ->group([
+                    'ProductOptionValueLists.option_id'
+                ])
+                ->where([]);
+
+            if ($search) {
                 $data->where([
-                    'ProductToCategories.product_category_id IN' => $children
-                ]);
-            } else {
-                $data->where([
-                    'ProductToCategories.product_category_id' => $category_id
-                ]);
+                    'MATCH (Products.name, Products.highlight) AGAINST (:search IN BOOLEAN MODE)'
+                ])
+                    ->bind(':search', $search, 'string');
             }
-        }
 
-        $data = $data
-            ->map(function(\App\Model\Entity\ProductOptionValueList $row) {
-                $values = array_unique(explode(',', $row->option_values));
-                $value_lists = [];
-                if (is_array($values)) {
-                    $valueEntities = $this->ProductOptionValueLists->OptionValues->find()
-                        ->where([
-                            'id IN' => $values
-                        ])
-                        ->toArray();
-                    foreach($valueEntities as $val) {
-                        array_push($value_lists, [
-                           //'option_id' => (int) $row->get('Options')['id'],
-                            'option_value_id' => $val->get('id'),
-                            'name' => $val->get('name')
-                        ]);
+            if ($min_price && $max_price) {
+                $data->where(function(QueryExpression $exp) use ($min_price, $max_price) {
+                    return $exp->between('price_sale', $min_price, $max_price);
+                });
+            }
+
+            if ($category_id) {
+                $descendants = $this->ProductCategories->find('children', ['for' => $category_id])
+                    ->toArray();
+                $children = Hash::extract($descendants, '{n}.id');
+                if ($children) {
+                    $data->where([
+                        'ProductToCategories.product_category_id IN' => $children
+                    ]);
+                } else {
+                    $data->where([
+                        'ProductToCategories.product_category_id' => $category_id
+                    ]);
+                }
+            }
+
+            $data = $data
+                ->map(function(\App\Model\Entity\ProductOptionValueList $row) {
+                    $values = array_unique(explode(',', $row->option_values));
+                    $value_lists = [];
+                    if (is_array($values)) {
+                        $valueEntities = $this->ProductOptionValueLists->OptionValues->find()
+                            ->where([
+                                'id IN' => $values
+                            ])
+                            ->toArray();
+                        foreach($valueEntities as $val) {
+                            array_push($value_lists, [
+                                //'option_id' => (int) $row->get('Options')['id'],
+                                'option_value_id' => $val->get('id'),
+                                'name' => $val->get('name')
+                            ]);
+                        }
+
                     }
 
-                }
+                    $row->values = $value_lists;
+                    unset($row->option_values);
+                    return $row;
+                })
+                ->toArray();
+        }
 
-                $row->values = $value_lists;
-                unset($row->option_values);
-                return $row;
-            })
-            ->toArray();
 
 
-        $this->set(compact('data'));
+
+        $this->set(compact('data', 'error'));
     }
 
     public function priceRange()
@@ -298,111 +316,142 @@ class ProductFiltersController extends Controller
         $this->set(compact('data'));
     }
 
+
+    /**
+     * @return Validator
+     */
+    protected function _validator()
+    {
+        $validator = new Validator();
+
+        $validator->numeric('min_price')
+            ->numeric('max_price')
+            ->range('min_price', [0, 100000000], 'harga minimal 0 - 100.000.000')
+            ->range('max_price', [1, 100000000], 'harga maxsimal 1 - 100.000.000')
+            ->greaterThanOrEqualToField('max_price', 'min_price', 'harga maksimal harus lebih besar dari harga minimal');
+
+        return $validator;
+    }
+
     public function index()
     {
         $search = $this->request->getQuery('q');
         $category_id = $this->request->getQuery('category_id');
+        $min_price = $this->request->getQuery('min_price');
+        $max_price = $this->request->getQuery('max_price');
 
-        $data = $this->Products->find()
-            ->select([
-                'id',
-                'name',
-                'slug',
-                'model',
-                'video_url',
-                'price',
-                'price_sale',
-                'highlight',
-                'profile',
-                'view',
-                'point',
-                'rating',
-                'rating_count',
-                'score' => "(MATCH(Products.name, Products.highlight) AGAINST(:search IN BOOLEAN MODE))",
-                'created'
-            ])
-            ->leftJoinWith('ProductToCategories')
-            ->where([
-                'Products.product_status_id' => 1
-            ]);
 
-        if ($search) {
-            $data->where([
-                'MATCH (Products.name, Products.highlight) AGAINST (:search IN BOOLEAN MODE)'
-            ]);
-        }
+        $validator = $this->_validator();
+        $error = $validator->errors($this->request->getQueryParams());
 
-        if ($category_id) {
-            $descendants = $this->ProductCategories->find('children', ['for' => $category_id])
-                ->toArray();
-            $children = Hash::extract($descendants, '{n}.id');
-            if ($children) {
-                $data->where([
-                    'ProductToCategories.product_category_id IN' => $children
+        if (!$error) {
+            $data = $this->Products->find()
+                ->select([
+                    'id',
+                    'name',
+                    'slug',
+                    'model',
+                    'video_url',
+                    'price',
+                    'price_sale',
+                    'highlight',
+                    'profile',
+                    'view',
+                    'point',
+                    'rating',
+                    'rating_count',
+                    'score' => "(MATCH(Products.name, Products.highlight) AGAINST(:search IN BOOLEAN MODE))",
+                    'created'
+                ])
+                ->leftJoinWith('ProductToCategories')
+                ->where([
+                    'Products.product_status_id' => 1
                 ]);
-            } else {
+
+            if ($search) {
                 $data->where([
-                    'ProductToCategories.product_category_id' => $category_id
+                    'MATCH (Products.name, Products.highlight) AGAINST (:search IN BOOLEAN MODE)'
                 ]);
             }
 
-        }
+            if ($min_price && $max_price) {
+                $data->where(function(QueryExpression $exp) use ($min_price, $max_price) {
+                    return $exp->between('price_sale', $min_price, $max_price);
+                });
+            }
+
+            if ($category_id) {
+                $descendants = $this->ProductCategories->find('children', ['for' => $category_id])
+                    ->toArray();
+                $children = Hash::extract($descendants, '{n}.id');
+                if ($children) {
+                    $data->where([
+                        'ProductToCategories.product_category_id IN' => $children
+                    ]);
+                } else {
+                    $data->where([
+                        'ProductToCategories.product_category_id' => $category_id
+                    ]);
+                }
+
+            }
 
 
-        $data = $data
-            ->bind(':search', $search, 'string')
-            ->contain([
-                'ProductImages' => [
-                    'fields' => [
-                        'name',
-                        'product_id',
-                        'idx',
-                    ],
-                    'sort' => ['ProductImages.primary' => 'DESC']
-                ],
-                'ProductTags' => [
-                    'Tags'
-                ],
-                'ProductOptionPrices' => [
-                    'fields' => [
-                        'id',
-                        'product_id',
-                        'sku',
-                        'expired',
-                        'price'
-                    ],
-                    'ProductOptionValueLists' => [
-                        'Options' => [
-                            'fields' => ['id','name']
+            $data = $data
+                ->bind(':search', $search, 'string')
+                ->contain([
+                    'ProductImages' => [
+                        'fields' => [
+                            'name',
+                            'product_id',
+                            'idx',
                         ],
-                        'OptionValues' => [
-                            'fields' => ['id','name']
-                        ]
+                        'sort' => ['ProductImages.primary' => 'DESC']
                     ],
-                    'ProductOptionStocks' => [
-                        'Branches' => [
-                            'fields' => [
-                                'id', 'name'
+                    'ProductTags' => [
+                        'Tags'
+                    ],
+                    'ProductOptionPrices' => [
+                        'fields' => [
+                            'id',
+                            'product_id',
+                            'sku',
+                            'expired',
+                            'price'
+                        ],
+                        'ProductOptionValueLists' => [
+                            'Options' => [
+                                'fields' => ['id','name']
+                            ],
+                            'OptionValues' => [
+                                'fields' => ['id','name']
+                            ]
+                        ],
+                        'ProductOptionStocks' => [
+                            'Branches' => [
+                                'fields' => [
+                                    'id', 'name'
+                                ]
                             ]
                         ]
                     ]
-                ]
-            ])
-            ->orderDesc('score');
+                ])
+                ->orderDesc('score');
 
-        $data = $this->paginate($data, [
-            'limit' => (int) $this->request->getQuery('limit', 5)
-        ])->map(function(\App\Model\Entity\Product $row) {
-            $images = [];
-            foreach($row->get('product_images') as $vl){
+            $data = $this->paginate($data, [
+                'limit' => (int) $this->request->getQuery('limit', 5)
+            ])->map(function(\App\Model\Entity\Product $row) {
+                $images = [];
+                foreach($row->get('product_images') as $vl){
 
-                $images[] = $vl['name'];
-            }
-            $row->images = $images;
-            //unset($row->product_images);
-            return $row;
-        });
+                    $images[] = $vl['name'];
+                }
+                $row->images = $images;
+                //unset($row->product_images);
+                return $row;
+            });
+        }
 
-        $this->set(compact('data'));
+        $this->set(compact('data', 'error'));
     }
 }
