58cd618a6d2948bf0a1aba4baf4ffbc2071c4d6f
rez <thinktobad@gmail.com>
2019-05-22 11:12:47 +0700

    flashsale

--- src/Controller/V1/FlashSaleController.php
+++ src/Controller/V1/FlashSaleController.php
@@ -138,4 +138,129 @@ class FlashSaleController extends Controller
         $this->set(compact('flashsale'));
     }
 
+    public function time(){
+        $timeList = $this->ProductDeals->find()
+            ->select([
+                'id',
+                'time' => 'TIME(date_start)',
+                'date_start'
+            ])
+            ->where(['status' => 1])
+            ->limit(3)
+            ->orderAsc('id');
+        $this->set(compact('timeList'));
+    }
+
+
+    public function byId($id = null){
+        $flashsale = $this->ProductDeals->find()
+            ->select([
+                'start' => 'ProductDeals.date_start',
+                'end' => 'ProductDeals.date_end',
+                'id'
+            ])
+            ->contain([
+                'ProductDealDetails' => [
+                    'fields' => [
+                        'product_deal_id',
+                        'product_id',
+                        'price_sale',
+                        'start_stock',
+                    ],
+                    'Products' => [
+                        'fields' => [
+                            'id',
+                            'name',
+                            'slug',
+                            'point',
+                            'price',
+                            'price_sale',
+
+                        ],
+                        'ProductImages' => [
+                            'fields' => [
+                                'name',
+                                'product_id',
+                            ],
+                            'sort' => ['ProductImages.primary' => 'DESC','ProductImages.created' => 'ASC']
+                        ],
+                        'ProductToCategories' => [
+                            'fields' => [
+                                'product_id',
+                                'product_category_id',
+                            ],
+                            'ProductCategories' => [
+                                'fields' => [
+                                    'name'
+                                ]
+                            ]
+                        ],
+                    ]
+                ]
+            ])
+            ->where(['ProductDeals.status' => 1, 'ProductDeals.id' => $id])
+            ->map(function (\App\Model\Entity\ProductDeal $row) {
+                foreach ($row['product_deal_details'] as $key => $vals){
+
+                    /* replace harga */
+                    $row->product_deal_details[$key]->product->price_sale = $row->product_deal_details[$key]->price_sale;
+
+                    /* discount percent */
+                    $percent = ( $row->product_deal_details[$key]->product->price - $row->product_deal_details[$key]->product->price_sale) / $row->product_deal_details[$key]->product->price * 100;
+                    $row->product_deal_details[$key]->product->percent = round($percent);
+
+                    /* SUM sisa Stock*/
+                    $query = $this->ProductOptionStocks->find();
+                    $sisa = $query
+                        ->select(['sum' => $query->func()->sum('ProductOptionStocks.stock')])
+                        ->where(['ProductOptionStocks.product_id' => $row->product_deal_details[$key]->product_id])->toArray();
+                    $row->product_deal_details[$key]->product->stockavailable = $sisa[0]['sum'];
+                    $row->product_deal_details[$key]->product->startstock = $row->product_deal_details[$key]->start_stock;
+
+                    /* cari jumlah penjualan */
+                    $queryOrder = $this->OrderDetailProducts->find();
+                    $terjual = $queryOrder
+                        ->contain([
+                            'OrderDetails'
+                        ])
+                        ->select(['sum' => $queryOrder->func()->sum('OrderDetailProducts.qty')])
+                        ->where([
+                            'OrderDetails.order_status_id IN' => ['2','3','4'],
+                            'OrderDetailProducts.product_id' => $row->product_deal_details[$key]->product_id,
+                        ])
+                        ->where(function(\Cake\Database\Expression\QueryExpression $exp) use($row){
+                            return $exp->between('OrderDetailProducts.created', $row->start, $row->end, 'datetime');
+                        })
+                        ->toArray();
+                    $row->product_deal_details[$key]->product->salestock = $terjual[0]['sum'] ? $terjual[0]['sum'] : 0;
+                    $barangTerjual = $row->product_deal_details[$key]->product->startstock - $row->product_deal_details[$key]->product->salestock;
+
+                    if($barangTerjual <= 10){
+                        $row->product_deal_details[$key]->product->noted = 'Segera Habis';
+                    }else{
+                        $row->product_deal_details[$key]->product->noted = $row->product_deal_details[$key]->product->salestock.' Barang Terjual';
+                    }
+
+                    /* Images*/
+                    $row->product_deal_details[$key]->product->images = Hash::extract($row->product_deal_details[$key]->product->product_images, '{n}.name');
+                    $row->product_deal_details[$key]->product->categories = $row->product_deal_details[$key]->product->product_to_categories[0]->product_category->name;
+
+                    unset($row->product_deal_details[$key]->product->product_to_categories);
+                    unset($row->product_deal_details[$key]->product->product_images);
+                    //unset($row->product_deal_details[$key]->product->id);
+                    unset($row->product_deal_details[$key]->product_id);
+                    unset($row->product_deal_details[$key]->product_deal_id);
+                    unset($row->product_deal_details[$key]->price_sale);
+                    unset($row->product_deal_details[$key]->start_stock);
+                    unset($row->id);
+                }
+
+                //$row->set('start', $row->end->timestamp);
+                //$row->set('end', $row->end->timestamp);
+                return $row;
+            })
+            ->first();
+
+        $this->set(compact('flashsale'));
+    }
 }
