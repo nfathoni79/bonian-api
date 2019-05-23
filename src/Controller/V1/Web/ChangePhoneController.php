83a88760204ba53a1e7c906e7f0b2a31c518f3b9
kinosuke <iwaninfo@gmail.com>
2019-05-23 18:25:00 +0700

    request change phone API

---
+++ src/Controller/V1/Web/ChangePhoneController.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller\V1\Web;
+
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\I18n\Time;
+use Cake\Utility\Hash;
+use Cake\Validation\Validator;
+use Cake\Cache\Cache;
+
+/**
+ * Customers controller
+ *
+ * @property \App\Model\Table\CustomersTable $Customers
+ * @link https://book.cakephp.org/3.0/en/controllers/pages-controller.html
+ */
+class ChangePhoneController extends AppController
+{
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadModel('Customers');
+    }
+
+    public function index()
+    {
+        $this->request->allowMethod('post');
+
+        $passwordEntity = $this->Customers->find()
+            ->select([
+                'id',
+                'password'
+            ])
+            ->where([
+                'id' => $this->Authenticate->getId()
+            ])
+            ->first();
+
+        $validator = new Validator();
+
+        $validator
+            ->add('password', 'verify_password', [
+            'rule' => function($value) use ($passwordEntity)  {
+                return (new DefaultPasswordHasher())->check($value, $passwordEntity->get('password'));
+            },
+            'message' => 'Password lama anda tidak valid'
+        ]);
+
+        $error = $validator->errors($this->request->getData());
+        if (!$error) {
+            $data = [
+                'session_id' => $this->request->getData('session_id', rand(1000, 9999)),
+                'step' => 2,
+            ];
+
+            Cache::write($data['session_id'], ['step' => 1, 'entity' => $passwordEntity], 'change_phone');
+
+
+        } else {
+            $this->response = $this->response->withStatus(406);
+        }
+
+
+        $this->set(compact('error', 'data'));
+    }
+
+
+    public function setPhone()
+    {
+        $this->request->allowMethod('post');
+
+        if ($session_id = $this->request->getData('session_id')) {
+            if (($cache = Cache::read($session_id, 'change_phone'))) {
+                $validator = new Validator();
+                $validator
+                    ->regex('phone', '/^(\+\d{11,13}|0\d{9,11})$/', 'Nomor handphone tidak valid')
+                    ->add('phone', 'is_unique', [
+                        'rule' => function($value)   {
+                            $value = preg_replace('/^0/i', '+62', $value);
+                            return $this->Customers->find()
+                                ->where([
+                                    'phone' => $value,
+                                    //'id !=' => $this->Authenticate->getId()
+                                ])
+                                ->count() == 0;
+                        },
+                        'message' => 'Nomor handphone sudah terdaftar.'
+                    ]);
+
+                $error = $validator->errors($this->request->getData());
+
+                if (!$error) {
+                    $data = [
+                        'session_id' => $this->request->getData('session_id', rand(1000, 9999)),
+                        'step' => 3,
+                    ];
+
+                    $cache['step'] = $data['step'];
+                    $cache['phone'] = preg_replace('/^0/i', '+62', $this->request->getData('phone'));
+
+                    $this->SendAuth->register('change-phone', $cache['phone']);
+                    $code = $this->SendAuth->generates();
+
+                    if (!$code) {
+                        $text = 'Demi keamanan, mohon TIDAK MEMBERIKAN kode kepada siapapun TERMASUK TIM ZOLAKU. Request perubahan handphone, Kode berlaku 15 mnt : '. $code;
+                        $this->Sms->send($cache['phone'], $text);
+                    }
+
+                    $cache['otp'] = $code;
+
+                    Cache::write($data['session_id'], $cache, 'change_phone');
+
+                } else {
+
+                    $this->response = $this->response->withStatus(406);
+                }
+
+            } else {
+                $this->response = $this->response->withStatus(406, 'No session id');
+            }
+        }
+
+        $this->set(compact('error', 'data'));
+    }
+
+    public function getStep($step) {
+
+    }
+
+    public function verification()
+    {
+        $this->request->allowMethod('post');
+        if ($session_id = $this->request->getData('session_id')) {
+            if (($cache = Cache::read($session_id, 'change_phone'))) {
+
+                $this->SendAuth->register('change-phone', $cache['phone']);
+
+                $validator = new Validator();
+                $validator
+                    ->add('otp', 'is_valid', [
+                    'rule' => function($value) {
+                        return $this->SendAuth->isValid($value);
+                    },
+                    'message' => 'kode OTP salah.'
+                ]);
+
+                $error = $validator->errors($this->request->getData());
+                if (!$error) {
+                    if (isset($cache['entity'])) {
+                        /**
+                         * @var \App\Model\Entity\Customer $customerEntity
+                         */
+                        $customerEntity = $cache['entity'];
+                        if ($customerEntity instanceof \App\Model\Entity\Customer) {
+                            $customerEntity->set('phone', $cache['phone']);
+                            if ($this->Customers->save($customerEntity)) {
+                                $data = [
+                                    'session_id' => $this->request->getData('session_id', rand(1000, 9999)),
+                                    'step' => 4,
+                                ];
+                                Cache::delete($data['session_id'], 'change_phone');
+                                $this->SendAuth->setUsed();
+                            }
+                        }
+                    }
+
+
+                } else {
+                    $this->response = $this->response->withStatus(406);
+                }
+            }
+        }
+        $this->set(compact('error', 'data'));
+    }
+
+}
