e7cd39d907063c848fb2b49bbd272cae355b5234
kinosuke <iwaninfo@gmail.com>
2019-09-02 13:15:55 +0700

    app exception

---
+++ src/Error/AppExceptionRenderer.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace App\Error;
+use Cake\Error\ExceptionRenderer;
+
+
+class AppExceptionRenderer extends ExceptionRenderer
+{
+
+    protected function errorTemplate(\Exception $error)
+    {
+        return json_encode([
+            'status' => 'ERROR',
+            'code' => $error->getCode(),
+            'message' => $error->getMessage()
+        ]);
+    }
+
+    function InvalidToken(\Exception $error)
+    {
+        $response = $this->controller->response;
+        return $response->withType('application/json')
+            ->withStatus($error->getCode(), $error->getMessage())
+            ->withStringBody($this->errorTemplate($error));
+    }
+
+
+    function MissingToken(\Exception $error)
+    {
+        $response = $this->controller->response;
+        return $response->withType('application/json')
+            ->withStatus($error->getCode(), $error->getMessage())
+            ->withStringBody($this->errorTemplate($error));
+    }
+
+
+    function ExpiredToken(\Exception $error)
+    {
+        $response = $this->controller->response;
+        return $response->withType('application/json')
+            ->withStatus($error->getCode(), $error->getMessage())
+            ->withStringBody($this->errorTemplate($error));
+    }
+
+    function MissingController(\Exception $error)
+    {
+        $response = $this->controller->response;
+        return $response->withType('application/json')
+            ->withStatus($error->getCode(), $error->getMessage())
+            ->withStringBody($this->errorTemplate($error));
+    }
+
+
+}
